868dd4e00190e9deafc92e83af35af3b8632ed43
Zachary Guard <zacharyguard@gmail.com>
2014-07-29 01:02:15 +0800

    Switch from tabs to spaces

--- public/js/results.js
+++ public/js/results.js
@@ -1,7 +1,7 @@
-Array.prototype.uniqueElems = function() {
+Array.prototype.uniqueElems = function () {
 	var elems = [];
 	var i;
-	for (i=0; i<this.length; i++) {
+	for (i = 0; i < this.length; i++) {
 		if (elems.indexOf(this[i]) === -1) {
 			elems.push(this[i]);
 		}
@@ -9,64 +9,64 @@ Array.prototype.uniqueElems = function() {
 	return elems.sort();
 };
 
-Array.prototype.occurences = function() {
+Array.prototype.occurences = function () {
 	var uniques = this.uniqueElems();
 	var counts = {};
 	var i;
-	
-	for (i=0; i<uniques.length; i++) {
+
+	for (i = 0; i < uniques.length; i++) {
 		counts[uniques[i]] = 0;
 	}
-	
-	for (i=0; i<this.length; i++) {
+
+	for (i = 0; i < this.length; i++) {
 		counts[this[i]] += 1;
 	}
-	
+
 	return counts;
 };
 
-Array.prototype.flatten = function() {
-	return [].concat.apply([],this);
+Array.prototype.flatten = function () {
+	return [].concat.apply([], this);
 };
 
 Chart.defaults.global.animation = false;
 Chart.defaults.global.tooltipFontFamily = "'Source Sans Pro', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif";
-Chart.defaults.global.tooltipFillColor  = "#e5e5e5";
+Chart.defaults.global.tooltipFillColor = "#e5e5e5";
 Chart.defaults.global.tooltipFontColor = "black";
 
 var params = location.href.parseURL("params");
 var slug = Object.keys(params)[0];
 
-xhr("results/" + slug + ".json", function(r) {
-    try {
-        r = JSON.parse(r);
-        showResults(r);
-    } catch (e) {
-        $("content").textContent = "There's nothing here ;_;";
-    }
+xhr("results/" + slug + ".json", function (r) {
+	try {
+		r = JSON.parse(r);
+		showResults(r);
+	} catch (e) {
+		$("content").textContent = "There's nothing here ;_;";
+	}
 });
 
 function showResults(r) {
-    var results = r.results;
-    var title = r.title;
-	
+	var results = r.results;
+	var title = r.title;
+
 	var questionsRow = document.createElement("tr");
-    for (i=0; i<results[0].length; i++) {
+	for (i = 0; i < results[0].length; i++) {
 		var questionHeader = document.createElement("th");
-        questionHeader.textContent = results[0][i].question;
+		questionHeader.textContent = results[0][i].question;
 		questionHeader.dataset.index = i;
 		questionHeader.dataset.type = results[0][i].type;
-        questionsRow.appendChild(questionHeader);
-    }
-    $("table").appendChild(questionsRow);
-    
-    for (i=0; i<results.length; i++) {
-        var answersRow = document.createElement("tr");
-        for (j=0; j<results[i].length; j++) {
+		questionsRow.appendChild(questionHeader);
+	}
+	$("table").appendChild(questionsRow);
+
+	for (i = 0; i < results.length; i++) {
+		var answersRow = document.createElement("tr");
+		for (j = 0; j < results[i].length; j++) {
 			var answerTD = document.createElement("td");
-			
+
 			if (results[i][j].type === "checkbox") {
-				for (k=0; k<results[i][j].answer.length; k++) {
+				for (k = 0; k < results[i][j].answer.length; k++) {
 					var endChar = "; ";
 					if (k === results[i][j].answer.length - 1) {
 						endChar = "";
@@ -76,56 +76,56 @@ function showResults(r) {
 			} else {
 				answerTD.textContent = results[i][j].answer;
 			}
-            answersRow.appendChild(answerTD);
-        }
-        $("table").appendChild(answersRow);
-    }
-    
-    var h2 = document.createElement("h2");
-    h2.innerHTML = "Results - <a href='survey?" + slug + "'>" + title.encodeHTML() + "</a>";
-    $("content").insertBefore(h2, $("table"));
-	
+			answersRow.appendChild(answerTD);
+		}
+		$("table").appendChild(answersRow);
+	}
+
+	var h2 = document.createElement("h2");
+	h2.innerHTML = "Results - <a href='survey?" + slug + "'>" + title.encodeHTML() + "</a>";
+	$("content").insertBefore(h2, $("table"));
+
 	showCharts(results);
-    
-    document.title = "Results - " + title + " - qwipi";
+
+	document.title = "Results - " + title + " - qwipi";
 }
 
 function showCharts(results) {
 	var chartQs = [];
 	var chartsDiv = document.querySelector("#charts");
 	var palette = ["#fa8072", "#fa72a8", "#fac472", "#fa72ec", "#72fa80", "#72ecfa", "#72a8fa", "#72fac4"];
-	
-	for (i=0; i<results[0].length; i++) {
+
+	for (i = 0; i < results[0].length; i++) {
 		chartQs.push({
 			title: results[0][i].question,
 			type: results[0][i].type,
 			answers: []
 		});
 	}
-	
-	for (i=0; i<results.length; i++) {
-		for (j=0; j<results[i].length; j++) {
+
+	for (i = 0; i < results.length; i++) {
+		for (j = 0; j < results[i].length; j++) {
 			chartQs[j].answers.push(results[i][j].answer);
 		}
 	}
-	
-	for (i=0; i<chartQs.length; i++) {
+
+	for (i = 0; i < chartQs.length; i++) {
 		if (chartQs[i].type === "radio" || chartQs[i].type === "checkbox") {
 			var flatAnswers = chartQs[i].answers.flatten();
-			
+
 			var uniques = flatAnswers.uniqueElems();
 			var counts = flatAnswers.occurences();
-			
+
 			var chartData = [];
-			
-			for (j=0; j<uniques.length; j++) {
+
+			for (j = 0; j < uniques.length; j++) {
 				chartData.push({
 					label: uniques[j],
 					value: counts[uniques[j]],
-					color: palette[j] || "rgb(" + Math.round(Math.random()*255) + "," + Math.round(Math.random()*255) + "," + Math.round(Math.random()*255) + ")"
+					color: palette[j] || "rgb(" + Math.round(Math.random() * 255) + "," + Math.round(Math.random() * 255) + "," + Math.round(Math.random() * 255) + ")"
 				});
 			}
-			
+
 			var chartContainer = document.createElement("div");
 			chartContainer.innerHTML = "<h3>" + chartQs[i].title + "</h3>";
 
